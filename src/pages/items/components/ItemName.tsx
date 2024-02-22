66b019b8364836c2939cf64c4171a6cec2d21c49
Garik Aghayan <11.aghayan.99@gmail.com>
2024-02-22 21:35:52 +0400

    word breaks added

--- src/pages/items/components/ItemName.tsx
+++ src/pages/items/components/ItemName.tsx
@@ -7,10 +7,10 @@ type Props = {
 const ItemName = ({ name }: Props) => {
   return (
     <div className="flex gap-2 justify-center border-b border-gray-900 pb-2">
-      <h3 className="text-gray-900 text-right text-xl font-medium border-r border-gray-900 pr-2">
+      <h3 className="text-gray-900 text-right text-xl font-medium border-r border-gray-900 pr-2 break-words">
         {name.am}
       </h3>
-      <h3 className="text-gray-900 text-xl font-medium">
+      <h3 className="text-gray-900 text-xl font-medium break-words">
         {name.ru}
       </h3>
     </div>
