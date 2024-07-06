31a40de584f745c9bdfdabfbce9680847a4e45db
Garik Aghayan <11.aghayan.99@gmail.com>
2024-07-07 03:33:24 +0400

    server address changed

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://zipit-server.onrender.com/api/v1';
+const BASE_URL = 'https://11aghayan-zipit-server-54e2.hostman.dev/api/v1';
 
 // const BASE_URL = 'http://localhost:3200/api/v1';
 
